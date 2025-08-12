8e427b783b28670edf0cf051cd78c7671cb1adae
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2025-08-12 13:20:24 +0200

    Rebase from 'debian/rolling/py_binding_tools'

--- test/test_basic.py
+++ test/test_basic.py
@@ -1,10 +1,46 @@
+#####################################################################
+# Software License Agreement (BSD License)
+#
+#  Copyright (c) 2024, Bielefeld University
+#  All rights reserved.
+#
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions
+#  are met:
+#
+#   * Redistributions of source code must retain the above copyright
+#     notice, this list of conditions and the following disclaimer.
+#   * Redistributions in binary form must reproduce the above
+#     copyright notice, this list of conditions and the following
+#     disclaimer in the documentation and/or other materials provided
+#     with the distribution.
+#   * The name of Robert Haschke may not be used to endorse or promote
+#     products derived from this software without specific prior
+#     written permission.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+#  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+#  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+#  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+#  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+#  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+#  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+#  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+#  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+#  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+#  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+#  POSSIBILITY OF SUCH DAMAGE.
+#####################################################################
+
+# Author: Robert Haschke
+
 import pytest
 import sys
 import time
 
 import rclpy
 import rclcpp
-from py_binding_tools_test import PubAndSub, inc
+from py_binding_tools_test import PubAndSub, inc, incTime
 
 from std_msgs.msg import Int32, Bool
 
@@ -76,6 +112,11 @@ def test_msg_conversion_failure():
         inc(Bool(data=False))
     with pytest.raises(TypeError):
         inc(42)
+
+
+def test_time_conversion():
+    result = incTime(rclpy.time.Time(seconds=1, nanoseconds=42))
+    assert result.nanoseconds == 1000000043
 
 
 if __name__ == "__main__":
